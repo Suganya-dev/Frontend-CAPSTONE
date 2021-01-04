b07b11aad04b2259bb5c075212c7b631756bbec5
Suganya <suganprakash0621@gmail.com>
2021-01-04 16:47:31 -0600

    done with react confetti

--- src/KidsDashBoard/KidActivityDetail.js
+++ src/KidsDashBoard/KidActivityDetail.js
@@ -1,13 +1,14 @@
-import React,{useContext} from "react"
+import React,{useContext,useState} from "react"
 import Button from 'react-bootstrap/Button'
 import {ActivityContext} from "../Activities/Activitydataprovider"
-import "./Kids.css"
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
+import "./Kids.css"
 
 
 export const KidActivityDetail = ({activityObj,activityType,rewardType}) => {
   const { width, height } = useWindowSize()
+  const[confetti,setConfetti] = useState(false)
   const{completeActivity} = useContext(ActivityContext)
   const kidId = localStorage.getItem("kidschorepad_user")
 
@@ -16,14 +17,23 @@ export const KidActivityDetail = ({activityObj,activityType,rewardType}) => {
     id:activityObj.id,
     isCompleted: true,
       }
-    completeActivity(completedActivityObject)
-    window.alert(`Congrats,
+      setConfetti(true)
+      completeActivity(completedActivityObject)
+      // console.log(confetti)
+      setTimeout(() => { window.alert(`Congrats,
     You earned ${rewardType.points} reward points`)
+      },1000)}
    
-    }
 
 return (
-<section className = "activity">
+  
+  <section className = "activity">
+<Confetti
+    className = "confetti"
+    run = {true}
+    width={width}
+    height={height}
+  />
 <h3 className="activity__name">Activity Name: {activityObj.name}</h3>
 <h3 className="activity__date">Date: {activityObj.date}</h3>
 <h3 className="activity__type">Activity Type: {activityType ? activityType.name: ""}</h3>
@@ -33,12 +43,7 @@ return (
 <section className="button">
 <Button  variant="primary"  className="button" type="submit" 
 onClick={() => 
-{completeCurrentActivity()
-  return(
-    <Confetti
-    width={width}
-    height={height}
-  />)}}
+{completeCurrentActivity()}}
 > Completed Activity </Button>
 </section>
 </section>)
